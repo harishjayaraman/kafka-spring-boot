b2c692dbf42a8581452de788b8133b7317318a97
Harish <harish@branchr.co>
2017-04-19 04:35:25 +0530

    Initial commit of spring-boot application

---
+++ src/main/java/com/kafka/KafkaSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringBootApplication.class, args);
+	}
+}
